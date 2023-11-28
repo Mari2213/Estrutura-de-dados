38b77515a143fa27b343b356c7493d8911f3b43f
Marianna Silva <mariannasilvapereira15@gmail.com>
2023-11-28 18:46:36 -0300

    Estrutura de Dados

--- src/Execicios/ExeSaladeAula5/Ex1/PriorityQueue.java
+++ src/Execicios/ExeSaladeAula5/Ex1/PriorityQueue.java
@@ -13,11 +13,11 @@ public class PriorityQueue {
         pneCount = 0;
     }
 
-    public boolean isEmpty(){
+    public boolean isEmpty(){ // Verifica se a fila está vazia
         return front == null;
     }
 
-    public void enqueue(int senha, boolean isPNE){
+    public void enqueue(int senha, boolean isPNE){ // adiciona um elemento à fila
         No newNo = new No(senha, isPNE);
 
         if (isEmpty()){
@@ -34,7 +34,7 @@ public class PriorityQueue {
         }
     }
 
-    public No dequeue(){
+    public No dequeue(){ // Remove e retorna o elemento da frente  da fila
         if (isEmpty()){
             System.out.println("Fila vazia");
             return null;
@@ -52,11 +52,11 @@ public class PriorityQueue {
         return temp;
     }
 
-    public boolean isTimeToServePNE(){
+    public boolean isTimeToServePNE(){ // Verifica se é hora de atender PNE
         return normalCount > 0 && normalCount % 3 == 0;
     }
 
-    public No serveNext(){
+    public No serveNext(){ // Atende o próximo da fila, respeitando a prioridade
         if (isEmpty()){
             System.out.println("Fila vazia");
             return null;
@@ -69,7 +69,7 @@ public class PriorityQueue {
         }
     }
 
-    public No dequeuePNE(){
+    public No dequeuePNE(){ // Remove e retorna o próximo PNE da fila
         if (front.isPNE){
             return dequeue();
         }
